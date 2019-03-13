b2b5d65e90adcd423fc02f02d2072e577a1d37cd
wentao <wentao.huang@sim.com>
2019-03-13 10:32:24 +0800

    [Android][build] add build script usage

---
+++ Android/build/mmbuild.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#author wentao
+#created 2013-03-13
+
+function usage() {
+  echo "  "
+  echo "==============================================================="
+  echo "# mmbuild optiones"
+  echo "# -------------------------------------------------------------"
+  echo " envSetup "
+  echo " "
+  echo " "
+  echo " buildCustom"
+  echo " "
+  echo "==============================================================="
+  echo "  "
+}
+
+
+#init 
+usage
+
+
